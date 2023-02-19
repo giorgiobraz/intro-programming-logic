d5e6c0c0e52f2417cca2507fcadff6266598bcca
Giorgio Braz <29463682+giorgiobraz@users.noreply.github.com>
2023-02-19 14:22:03 +0000

    caso de teste pronto

---
+++ q1.py
@@ -0,0 +1,10 @@
+'''
+TODO
+Escreva um programa que mostre a frase: "hello world" na tela do computador.
+💡 DICA: use a função "print()" do Python.
+'''
+
+
+def hello_world():
+    pass
+    # escreva a solução aqui...
---
+++ q1_test.py
@@ -0,0 +1,9 @@
+'''
+Escreva um programa que mostre a frase: "q1 world!" na tela do computador.
+💡 DICA: use a função "print()" do Python.
+'''
+
+import q1
+
+def test_q1():
+    assert q1.hello_world() == "hello world"
